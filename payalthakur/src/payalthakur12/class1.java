6785b34def01604a8c5f0ba6181abcb7d877ed65
Swapn <Swapn@Swapnil>
2022-08-02 08:13:00 +0530

    first time testing

---
+++ payalthakur/src/payalthakur12/class1.java
@@ -0,0 +1,22 @@
+package payalthakur12;
+
+public class class1 
+{
+
+
+
+public int sum a;
+a=13;
+return a;
+}
+{
+system.printout.ln("my name is swapnil");
+}
+public static main void (string  args[])
+{
+	class 1 ab = new class 1;
+
+	
+	
+	
+}
